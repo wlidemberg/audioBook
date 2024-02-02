0f13d2d84b2f32dfe93c5c9ac3a54c21eab0a03e
Berg Sousa <automatizei@gmail.com>
2024-02-02 16:19:22 -0300

    Icluinclusion of sound controls and video progress bar

--- script.js
+++ script.js
@@ -1,73 +1,113 @@
-const botaoPlayPause =  document.getElementById('play_pause');
-const audioCapitulo =   document.getElementById('audio_chapter');
-const botaoProximo =    document.getElementById('previous');
-const botaoAnterior =   document.getElementById('next');
-const nomeCapitulo = document.getElementById('chapter');
-let capituloAtual = 1;
+//document.addEventListener('DOMContentLoaded', function(){
 
-const numeroCapitulos = 10;
-let estaTocando = false;
 
-/*Função que faz tocar o audio*/
-function tocarFaixa(){
-    audioCapitulo.play();
-    estaTocando = true;
-    botaoPlayPause.classList.remove('bi-play-btn-fill');
-    botaoPlayPause.classList.add('bi-pause-btn-fill');
+    const botaoPlayPause =  document.getElementById('play_pause');
+    const audioCapitulo =   document.getElementById('audio_chapter');
+    const botaoProximo =    document.getElementById('previous');
+    const botaoAnterior =   document.getElementById('next');
+    const nomeCapitulo = document.getElementById('chapter');
+    const volumeControl = document.getElementById('volume_control');
+    const progressBar = document.getElementById('progress_bar')
+    let capituloAtual = 1;
+
+    const numeroCapitulos = 10;
+    let estaTocando = false;
+
+
+    volumeControl.addEventListener('input', () => {
+        audioCapitulo.volume = volumeControl.value / 100;
+    })
+
+    audioCapitulo.addEventListener('timeupdate', () => {
+        const percentualConcluido = (audioCapitulo.currentTime / audioCapitulo.duration) * 100;
+        progressBar.style.width = percentualConcluido + '%';
+    })
+
+    progressBar.addEventListener('click', (event) => {
+        const larguraTotal = progressBar.clientWidth;
+        const clickX = event.offsetX;
+        const novoTempo = (clickX / larguraTotal) * audioCapitulo.duration;
+
+        audioCapitulo.currentTime = novoTempo;
+    });
     
-}
+    /*progressBar.addEventListener('click', (event) => {
+        const larguraTotal = progressBar.clientWidth;
+        const x = event.offsetX;
+        const novoTempo = (x / larguraTotal) * audioCapitulo.duration;
+        audioCapitulo.currentTime = novoTempo;
+    })*/
 
-/*Função que pausa a faixa*/
-function pausarFaixa() {
-    audioCapitulo.pause();
-    estaTocando = false;
-    botaoPlayPause.classList.remove('bi-pause-btn-fill');
-    botaoPlayPause.classList.add('bi-play-btn-fill')
-}
+    /*audioCapitulo.addEventListener('timeupdate', () => {
+        const percentualConcluido = (audioCapitulo.currentTime / audioCapitulo.duration) * 100;
+        progressBar.style.width = percentualConcluido + '%';
+    });*/
 
-/*Função que verifica se esta tocando ou não*/
-function tocarPausar () {
-    if( estaTocando === false ) {
-        tocarFaixa();
-    } else {
-        pausarFaixa();
+
+    /*Função que faz tocar o audio*/
+    function tocarFaixa(){
+        audioCapitulo.play();
+        estaTocando = true;
+        botaoPlayPause.classList.remove('bi-play-btn-fill');
+        botaoPlayPause.classList.add('bi-pause-btn-fill');
+        
     }
-}
 
-// Função troca nome capitulo
-const trocarNomeFaixa = () => {
-    nomeCapitulo.innerText = "Capitulo " + capituloAtual;
-}
-
-/*Função para proxima faixa*/
-function proximaFaixa(){
-    if( capituloAtual === numeroCapitulos ){
-        capituloAtual = 1;
-    } else {
-        capituloAtual = capituloAtual + 1;
+    /*Função que pausa a faixa*/
+    function pausarFaixa() {
+        audioCapitulo.pause();
+        estaTocando = false;
+        botaoPlayPause.classList.remove('bi-pause-btn-fill');
+        botaoPlayPause.classList.add('bi-play-btn-fill')
     }
-    
-    //Atualizando a url do audio
-    audioCapitulo.src = "./books/dom-casmurro/"+ capituloAtual +".mp3";
-    audioCapitulo.play();
-    trocarNomeFaixa();
-    
-}
 
-// Função para Faixa anterior
-function anteriorFaixa(){
-    if( capituloAtual === 1 ){
-        capituloAtual = numeroCapitulos;
-    } else {
-        capituloAtual = capituloAtual - 1;
+    /*Função que verifica se esta tocando ou não*/
+    function tocarPausar () {
+        if( estaTocando === false ) {
+            tocarFaixa();
+        } else {
+            pausarFaixa();
+        }
     }
-    audioCapitulo.src = "./books/dom-casmurro/"+ capituloAtual +".mp3";
-    audioCapitulo.play();
-    trocarNomeFaixa();
-}
+
+    // Função troca nome capitulo
+    const trocarNomeFaixa = () => {
+        nomeCapitulo.innerText = "Capitulo " + capituloAtual;
+    }
+
+    /*Função para proxima faixa*/
+    function proximaFaixa(){
+        if( capituloAtual === numeroCapitulos ){
+            capituloAtual = 1;
+        } else {
+            capituloAtual = capituloAtual + 1;
+        }
+        
+        //Atualizando a url do audio
+        audioCapitulo.src = "./books/dom-casmurro/"+ capituloAtual +".mp3";
+        audioCapitulo.play();
+        trocarNomeFaixa();
+        
+    }
+
+    // Função para Faixa anterior
+    function anteriorFaixa(){
+        if( capituloAtual === 1 ){
+            capituloAtual = numeroCapitulos;
+        } else {
+            capituloAtual = capituloAtual - 1;
+        }
+        audioCapitulo.src = "./books/dom-casmurro/"+ capituloAtual +".mp3";
+        audioCapitulo.play();
+        trocarNomeFaixa();
+    }
 
 
 
-botaoPlayPause.addEventListener('click', tocarPausar);
-botaoProximo.addEventListener('click', proximaFaixa);
-botaoAnterior.addEventListener('click', anteriorFaixa);
+    botaoPlayPause.addEventListener('click', tocarPausar);
+    botaoProximo.addEventListener('click', proximaFaixa);
+    botaoAnterior.addEventListener('click', anteriorFaixa);
+
+
+
+//})
